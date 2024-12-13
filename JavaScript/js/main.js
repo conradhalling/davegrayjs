b9989a2b6bb37f886fc0eb0cc8fa42d196d48367
conradhalling <conrad.halling@icloud.com>
2024-12-12 21:06:26 -0500

    JavaScript
    Refactor the JavaScript code for the rock, scissors, or paper game.
    
    Update EOLs for main.css and index.html.

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,14 +1,12 @@
 // Dave Gray's JavaScript tutorial at https://courses.davegray.codes/.
 // Uncomment code to execute it.
 
-
 // Chapter 2 Link JavaScript to HTML (7:44)
 
 // console.log("Hello World!");
 // console.log(typeof "Dave");
 // console.log(typeof 42);
 // console.log(typeof true);
-
 
 // Chapter 3 Strings (15:17)
 
@@ -26,7 +24,6 @@
 // console.log(myVariable.split("e"));
 // console.log(myVariable.split(""));
 // console.log("Every good boy does fine".split(" "));
-
 
 // Chapter 4 Numbers (22:14)
 
@@ -64,7 +61,6 @@
 // console.log(Number.isNaN("Dave"));          // false
 // console.log(isNaN("Dave"));                 // true
 
-
 // Chapter 5 Math Methods (27:55)
 
 // console.log(Math.PI);                           // 3.141592653589793
@@ -78,13 +74,11 @@
 // console.log(Math.random())                      // [0, 1)
 // console.log(Math.floor(Math.random() * 10) + 1) //  A random number from 1 .. 10
 
-
 // Chapter 6 Code Challenge (32:59)
 
 // // Write code that returns a random letter from a name.
 // const name = "Dave"
 // console.log(name.charAt(Math.floor(Math.random() * name.length)))
-
 
 // Chapter 7 If Statements (40:07)
 
@@ -157,7 +151,6 @@
 //     }
 // }
 
-
 // Chapter 8 Switch Statements (46:11)
 
 // // A simple switch statement.
@@ -228,7 +221,6 @@
 //         console.log("Algorithm error!");
 // }
 
-
 // Chapter 9 Ternary Operator (49:40)
 
 // let soup = "Chicken Noodle Soup";
@@ -262,7 +254,6 @@
 //           ? "Computer wins!" : "PlayerOne wins!";
 // console.log(result);
 
-
 // Chapter 10 User Input (54:39)
 
 // // alert()
@@ -289,7 +280,6 @@
 // } else {
 //   console.log("You didn't enter your name.");
 // }
-
 
 // Chapter 11 Your First Game (1:04:38)
 
@@ -363,7 +353,6 @@
 //   }
 // }
 
-
 // Chapter 12 Loops (1:20:26)
 
 // // while loops
@@ -423,7 +412,6 @@
 //   counter += 1;
 // }
 
-
 // Chapter 13 Functions (1:36:20)
 
 // // Create and call a simple function.
@@ -457,7 +445,6 @@
 // console.log(toProperCase("dAvE"));
 // console.log(toProperCase("september"));
 
-
 // Chapter 14 Scope (1:48:10)
 
 // var x = 1;
@@ -487,7 +474,6 @@
 // console.log(`global: ${x}`);
 // console.log(`global: ${y}`);
 // console.log(`global: ${z}`);
-
 
 // Chapter 15 Arrays (2:05:29)
 
@@ -598,6 +584,201 @@
 // console.log(store[0][0][1]);
 // console.log(store[1][1][0]);
 
-
 // Chapter 16 Refactor the Game(2: 33: 48);
 // Use loops, functions, and arrays to refactor the rock-paper-scissors game.
+
+// A simple interactive game of rock, paper, or scissors.
+playGame();
+
+/**
+ * Play a game of rock, paper, or scissors.
+ */
+function playGame() {
+  let playGame = askToPlay();
+  while (playGame === true) {
+    let validPlayerChoice;
+    let playerChoice;
+    [playGame, validPlayerChoice, playerChoice] = getPlayerChoice();
+    if (validPlayerChoice) {
+      const computerChoice = getComputerChoice();
+      const wonLostOrTiedStatus = getWonLostOrTiedStatus(
+        playerChoice,
+        computerChoice
+      );
+      const wonLostOrTiedString = getWonLostOrTiedString(wonLostOrTiedStatus);
+      reportGameResult(playerChoice, computerChoice, wonLostOrTiedString);
+      playGame = askToPlayAgain();
+    }
+  } // while playGame === true
+  return;
+}
+
+/**
+ * Ask whether the user wants to play.
+ * @returns {boolean} true if user wants to play
+ * @example playGame = askToPlay();
+ */
+function askToPlay() {
+  const playGame = confirm("Shall we play rock, paper, or scissors?");
+  if (!playGame) {
+    alert("OK, maybe next time.");
+  }
+  return playGame;
+}
+
+/**
+ * Ask whether the user wants to play again.
+ * @returns {boolean} true if user wants to play again
+ * @example playGame = askToPlayAgain();
+ */
+function askToPlayAgain() {
+  const playGame = confirm("Play again?");
+  if (!playGame) {
+    alert("OK, thanks for playing!");
+  }
+  return playGame;
+}
+
+/**
+ * Show an alert reporting the result of the game.
+ * @param {string} playerChoice Player's choice of "rock", "paper", or "scissors"
+ * @param {string} computerChoice Computer's choice of "rock", "paper", or "scissors"
+ * @param {string} wonLostOrTiedString Whether the player won, lost, or tied
+ */
+function reportGameResult(playerChoice, computerChoice, wonLostOrTiedString) {
+  alert(
+    `You chose ${playerChoice}.\n` +
+      `The computer chose ${computerChoice}.\n` +
+      `${wonLostOrTiedString}`
+  );
+  return;
+}
+
+/**
+ * Given a won, tied, or lost status, return a string describing whether the
+ * player won, lost, or tied.
+ * @param   {string} wonLostOrTiedStatus One of "win", "lose", or "tie".
+ * @returns {string}
+ */
+function getWonLostOrTiedString(wonLostOrTiedStatus) {
+  let wonLostOrTiedString;
+  switch (wonLostOrTiedStatus) {
+    case "win":
+      wonLostOrTiedString = "You won!";
+      break;
+    case "lose":
+      wonLostOrTiedString = "You lost!";
+      break;
+    case "tie":
+      wonLostOrTiedString = "You tied!";
+      break;
+    default:
+      wonLostOrTiedString = "";
+  }
+  return wonLostOrTiedString;
+}
+
+/**
+ * Given the choices of the player and the computer, determine whether the
+ * player won, lost, or tied.
+ * @param {string} player One of "rock", "scissors", or "paper"
+ * @param {string} computer One of "rock", "scissors", or "paper"
+ * @returns {string} One of "win", "lose", or "tie"
+ */
+function getWonLostOrTiedStatus(playerChoice, computerChoice) {
+  let wonLostOrTiedStatus;
+  switch (playerChoice) {
+    case "rock":
+      switch (computerChoice) {
+        case "scissors":
+          wonLostOrTiedStatus = "win";
+          break;
+        case "paper":
+          wonLostOrTiedStatus = "lose";
+          break;
+        case "rock":
+          wonLostOrTiedStatus = "tie";
+          break;
+        default:
+          wonLostOrTiedStatus = "computer choice not found";
+      }
+      break;
+
+    case "paper":
+      switch (computerChoice) {
+        case "scissors":
+          wonLostOrTiedStatus = "lose";
+          break;
+        case "paper":
+          wonLostOrTiedStatus = "tie";
+          break;
+        case "rock":
+          wonLostOrTiedStatus = "win";
+          break;
+        default:
+          wonLostOrTiedStatus = "computer choice not found";
+      }
+      break;
+
+    case "scissors":
+      switch (computerChoice) {
+        case "scissors":
+          wonLostOrTiedStatus = "tie";
+          break;
+        case "paper":
+          wonLostOrTiedStatus = "win";
+          break;
+        case "rock":
+          wonLostOrTiedStatus = "lose";
+          break;
+        default:
+          wonLostOrTiedStatus = "computer choice not found";
+      }
+      break;
+
+    default:
+      wonLostOrTiedStatus = "player choice not found";
+  }
+  return wonLostOrTiedStatus;
+}
+
+/**
+ * Get the player's choice of "rock", "paper", "scissors".
+ * Return an array containing values for playGame, validChoice, and
+ * playerChoice.
+ * @returns {array} [0] playGame boolean, [1] validChoice boolean,
+ * [2] playerChoice string "rock", "paper", or "scissors"; or undefined
+ * @example [playGame, validChoice, playerChoice] = getPlayerChoice();
+ */
+function getPlayerChoice() {
+  const returnValues = [false, false, undefined];
+  const rawPlayerChoice = prompt("Please enter rock, paper, or scissors.");
+  if (rawPlayerChoice === null) {
+    alert("I guess you changed your mind. Maybe next time.");
+  } else {
+    returnValues[0] = true; // playGame
+    const playerChoice = rawPlayerChoice.trim().toLowerCase();
+    if (
+      playerChoice !== "rock" &&
+      playerChoice !== "paper" &&
+      playerChoice !== "scissors"
+    ) {
+      alert(`Your choice "${rawPlayerChoice}" is not valid.`);
+    } else {
+      returnValues[1] = true; // validChoice
+      returnValues[2] = playerChoice; // playerChoice
+    }
+  }
+  return returnValues;
+}
+
+/**
+ * Return a random choice of "rock", "paper", or "scissors".
+ * @returns {string}
+ */
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let index = Math.floor(Math.random() * 3);
+  let computerChoice = choices[index];
+  return computerChoice;
+}
